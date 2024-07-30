613ce1698b8740012c2f4cffb42c0f2e0c5b0a19
Dana Silver <dsilver1221@gmail.com>
2024-07-29 22:33:23 -0700

    Add local superuser password to migration.

--- tripreports/migrations/0001_create_superuser.py
+++ tripreports/migrations/0001_create_superuser.py
@@ -12,13 +12,17 @@ from google.cloud import secretmanager
 
 
 def createsuperuser(apps: StateApps, schema_editor: DatabaseSchemaEditor) -> None:
-    client = secretmanager.SecretManagerServiceClient()
+    if os.environ.get('GOOGLE_CLOUD_PROJECT', None):
+        client = secretmanager.SecretManagerServiceClient()
 
-    _, project = google.auth.default()
+        _, project = google.auth.default()
 
-    PASSWORD_NAME = os.environ.get("PASSWORD_NAME", "superuser_password")
-    name = f"projects/{project}/secrets/{PASSWORD_NAME}/versions/latest"
-    admin_password = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+        PASSWORD_NAME = os.environ.get("PASSWORD_NAME", "superuser_password")
+        name = f"projects/{project}/secrets/{PASSWORD_NAME}/versions/latest"
+        admin_password = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+    else:
+        # For local development
+        admin_password = "password"
 
     User.objects.create_superuser("admin", password=admin_password.strip())
 
